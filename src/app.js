55ccec73f6cebd07a539b7ae6d5ad74a60122623
Chhaya <tyagi.chhaya4@gmail.com>
2025-05-28 11:29:40 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app=express();
 app.use("/",(req,res)=>{
     res.send("hello chhaya to your new life from today on");
 });
+app.use("/chhaya",(req,res)=>{
+    res.send("bubu");
+})
 app.listen(7777,()=>{
     console.log(`Server is listening on port 7777`);
     
